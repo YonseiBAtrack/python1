ac009c78bd510fc43e7c93630043ef7bef08502f
minamotoyuki <s.heewon2@gmail.com>
2023-04-04 14:44:35 +0900

    python_baekjoon

---
+++ main.py
@@ -0,0 +1,62 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#2557
+print('Hello World!')
+
+#1000
+a, b = map(int, input().split())
+print(a+b)
+
+#1001
+a, b = map(int, input().split())
+print(a-b)
+
+#10998
+a, b = map(int, input().split())
+print(a*b)
+
+#1008
+a, b = map(int, input().split())
+print(a/b)
+
+#10869
+a, b = map(int, input().split())
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
+
+#10926
+a=input()
+
+print(str(a)+'??!')
+
+#18108
+y=input()
+
+print(int(y)-543)
+
+#10430
+a,b,c=map(int,input().split())
+
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
